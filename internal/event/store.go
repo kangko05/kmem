bc7af364935d69764307f2342b0a2dd0b86f2822
kang@endv <kyungohkang126@gmail.com>
2025-05-15 20:59:58 +0900

    added worker pool (8 workers default)

--- internal/event/store.go
+++ internal/event/store.go
@@ -4,29 +4,49 @@ import (
 	"context"
 	"kmem/internal/database"
 	"log"
+	"sync"
 )
 
 type Store struct {
-	ctx    context.Context
-	evchan chan eventType
-	pg     *database.Postgres
+	ctx     context.Context
+	evchan  chan eventType
+	pg      *database.Postgres
+	workers int
 }
 
 func NewStore(ctx context.Context, pg *database.Postgres) *Store {
 	return &Store{
-		ctx:    ctx,
-		evchan: make(chan eventType, 100),
-		pg:     pg,
+		ctx:     ctx,
+		evchan:  make(chan eventType, 100),
+		pg:      pg,
+		workers: 8, // TODO: default 8 for now
 	}
 }
 
 func (s *Store) Run() {
 	log.Println("starting event store...")
 
+	var wg sync.WaitGroup
+	wg.Add(s.workers)
+
+	for i := range s.workers {
+		go s.runWorker(i, &wg)
+	}
+
+	<-s.ctx.Done()
+	log.Println("context canceld, waiting for workers to stop...")
+
+	wg.Wait()
+	log.Println("all workers stopped, closing event store...")
+}
+
+func (s *Store) runWorker(id int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
 	for {
 		select {
 		case <-s.ctx.Done():
-			log.Println("closing event store...")
+			log.Printf("stopping worker %d", id)
 			return
 
 		case ev := <-s.evchan:
--- tests/databse_test.go
+++ tests/databse_test.go
@@ -1,37 +1,48 @@
 package tests
 
-// func TestPostgres(t *testing.T) {
-// 	err := godotenv.Load("../.env")
-// 	assert.Nil(t, err)
-//
-// 	t.Run("testConnection", func(t *testing.T) {
-// 		pg, err := database.Connect(t.Context())
-// 		if err != nil {
-// 			t.Fatal(err)
-// 		}
-// 		defer pg.Close()
-// 	})
-// }
-//
-// func TestQuery(t *testing.T) {
-// 	err := godotenv.Load("../.env")
-// 	assert.Nil(t, err)
-//
-// 	t.Run("test query user", func(t *testing.T) {
-// 		ctx, cancel := context.WithCancel(context.Background())
-//
-// 		pg, err := database.Connect(ctx)
-// 		if err != nil {
-// 			t.Fatal(err)
-// 		}
-// 		defer pg.Close()
-//
-// 		user, err := query.QueryUser(pg, "guest")
-//
-// 		assert.Nil(t, err)
-// 		assert.Equal(t, "guest", user.Username)
-// 		assert.True(t, utils.CheckPasswordHash("guestpass", user.Password))
-//
-// 		cancel()
-// 	})
-// }
+import (
+	"context"
+	"kmem/internal/database"
+	"kmem/internal/database/query"
+	"kmem/internal/utils"
+	"testing"
+
+	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPostgres(t *testing.T) {
+	err := godotenv.Load("../.env")
+	assert.Nil(t, err)
+
+	t.Run("testConnection", func(t *testing.T) {
+		pg, err := database.Connect(t.Context())
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer pg.Close()
+	})
+}
+
+func TestQuery(t *testing.T) {
+	err := godotenv.Load("../.env")
+	assert.Nil(t, err)
+
+	t.Run("test query user", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.Background())
+
+		pg, err := database.Connect(ctx)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer pg.Close()
+
+		user, err := query.QueryUser(pg, "guest")
+
+		assert.Nil(t, err)
+		assert.Equal(t, "guest", user.Username)
+		assert.True(t, utils.CheckPasswordHash("guestpass", user.Password))
+
+		cancel()
+	})
+}
