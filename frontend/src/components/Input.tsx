b99922a75bf62994f8029f83d0190a793e2be46c
kang@endv <kyungohkang126@gmail.com>
2025-05-27 23:29:01 +0900

    moving

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -35,8 +35,8 @@ export const FileInput = ({
   onChange: (ev: ChangeEvent<HTMLInputElement>) => void;
 }) => {
   return (
-    <div className="w-[90%] sm:w-full max-w-md mx-auto bg-white">
-      <label className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:bg-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:border-gray-600 transition-colors duration-200">
+    <div className="w-[90%] sm:w-full max-w-md mx-auto">
+      <label className="flex flex-col items-center justify-center w-full h-32 sm:h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:bg-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:border-gray-600 transition-colors duration-200">
         <div className="flex flex-col items-center justify-center pt-5 pb-6">
           <Upload className="w-10 h-10 mb-3 text-gray-400" />
           <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
